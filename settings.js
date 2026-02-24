bf84dc303c3b9910baa717b78bff2c0713db389d
zahid53402 <zahid.ali.00347@gmail.com>
2026-02-24 16:53:16 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝚉𝙰𝙷𝙸𝙳 𝙺𝙸𝙽𝙶',
+  author: '‎',
+  botName: "𝚉𝙰𝙷𝙸𝙳 𝙺𝙸𝙽𝙶",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '923044154575', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/zahid53402/ZAHID.KING/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
